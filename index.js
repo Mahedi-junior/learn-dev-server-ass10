b8179239e80eaad0e5fad5aa6920c6440a09d248
Mahedi-junior <mahedih.cse@gmail.com>
2022-12-07 14:13:29 +0600

    category data added

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 const courses = require("./data/courses.json");
+const category = require("./data/categories.json");
 
 app.get("/", (req, res) => {
   res.send("Learn Dev API is Running");
 });
 
+app.get("/course-category", (req, res) => {
+  res.send(category);
+});
+
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_course = courses.filter((course) => course.id === id);
+  res.send(category_course);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find((course) => course.id === id);
+  res.send(selectedCourses);
 });
 
 app.listen(port, () => {
